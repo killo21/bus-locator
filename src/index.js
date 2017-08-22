a17f5893df0839e2356bdddff06459b6e6d3228e
lshlyapnikov <lshlyapnikov@gmail.com>
2017-08-21 23:15:12 -0400

    adding react-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import App from './App';
+import BusStopForm from './BusStopForm';
+import RouteForm from './RouteForm';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App name = "Dmitry"/>, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/add-route" component={RouteForm} />
+      <Route exact path="/add-bus-stop" component={BusStopForm} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
